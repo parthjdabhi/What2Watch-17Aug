434f52d4c9de3e195f25b9cf9a1e45e95609d055
Parth Dabhi (iOS & Android developer) <iparth@iMac.local>
2016-08-17 10:58:53 +0530

    Last Swiped Movie

---
+++ What2Watch/MovieDescriptionViewController.swift
@@ -0,0 +1,26 @@
+//
+//  MovieDescriptionViewController.swift
+//  What2Watch
+//
+//  Created by Dustin Allen on 8/16/16.
+//  Copyright © 2016 Harloch. All rights reserved.
+//
+
+import Foundation
+
+class MovieDescriptionViewController: UIViewController {
+    
+    @IBOutlet var poster: UIImageView!
+    static var movieID = ""
+    
+    override func viewDidLoad() {
+        
+        /*
+        let imdbIDPoster : String = String(MainScreenViewController.imdbID)
+        let posterURL = "http://img.omdbapi.com/?i=\(imdbIDPoster)&apikey=57288a3b&h=1000"
+        poster.image = posterURL
+        */
+        
+    }
+
+}
